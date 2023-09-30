fa83d419810b66a45feef7eec08f83224a5922ef
Samundra Acharya <075bei032.samundra@pcampus.edu.np>
2023-09-30 15:18:15 +0545

    creating model

---
+++ model/blogModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+//  const Schema = mongoose.Schema
+//  const userSchema=new Schema({})
+const blogSchema = new mongoose.Schema({
+    title : {
+        type : String,
+        //required : true
+    },
+
+    subTitle : {
+        type : String
+    },
+
+    Description : {
+        type : String
+    }
+
+},
+{
+    timestamps:true  //created at and updated at field will be created automatically by mongoDB
+})
+
+
+const Blog = mongoose.model("Blog", blogSchema)
+module.exports=Blog;
+                //Alternative
+//module.exports = mongoose.model("Blog",blogSchema);
+
